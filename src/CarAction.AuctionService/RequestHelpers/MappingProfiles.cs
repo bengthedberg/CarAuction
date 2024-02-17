9e92e59349ccd290ee96984291b3d6fb46ff0eaa
Bengt Hedberg <bengt.hedberg@gmail.com>
2024-02-17 21:12:53 +1000

    Add data transfer objects and mapping configuration using automapper.

---
+++ src/CarAction.AuctionService/RequestHelpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CarAction.AuctionService.DTOs;
+using CarAction.AuctionService.Entities;
+
+namespace CarAction.AuctionService.RequestHelpers;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Auction, AuctionDTO>().IncludeMembers(x => x.Item);
+        CreateMap<Item, AuctionDTO>();
+        CreateMap<CreateAuctionDTO, Auction>()
+            .ForMember(d => d.Item, opt => opt.MapFrom(s => s));
+        CreateMap<CreateAuctionDTO, Item>();
+    }
+}
