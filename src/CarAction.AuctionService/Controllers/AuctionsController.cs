234ccacef11357018aa744106e4ff5de63e3651c
Bengt Hedberg <bengt.hedberg@gmail.com>
2024-02-18 09:57:36 +1000

    Add a controller for the auction service

---
+++ src/CarAction.AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+
+using CarAction.AuctionService.Data;
+using CarAction.AuctionService.DTOs;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarAction.AuctionService.Controller;
+
+[ApiController]
+[Route("api/auctions")]
+public class AuctionsController : ControllerBase
+{
+    private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDTO>>> GetAllAuctions()
+    {
+        var auctions = await _context.Auctions
+        .Include(x => x.Item)
+        .OrderBy(x => x.Item.Make)
+        .ToListAsync();
+
+        return _mapper.Map<List<AuctionDTO>>(auctions);
+
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDTO>> GetAuctionById(Guid id)
+    {
+        var auction = await _context.Auctions
+        .Include(x => x.Item)
+        .FirstOrDefaultAsync(x => x.Id == id);
+
+        return (auction == null)
+            ? NotFound()
+            : _mapper.Map<AuctionDTO>(auction);
+    }
+
+
+}
