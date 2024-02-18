d974d20cdb4c106a213e23766b4a1d750b7635e0
Bengt Hedberg <bengt.hedberg@gmail.com>
2024-02-18 17:12:55 +1000

    Implement Search Controller

---
+++ src/CarAction.SearchService/Controllers/SearchController.cs
@@ -0,0 +1,30 @@
+﻿using CarAction.SearchService.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+using MongoDB.Entities;
+
+namespace CarAction.SearchService.Controller;
+
+[ApiController]
+[Route("api/search")]
+public class SearchController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItems(string searchTerm)
+    {
+        var query = DB.Find<Item>();
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+
+        var result = await query.ExecuteAsync();
+
+        return Ok(result);
+    }
+
+
+}
