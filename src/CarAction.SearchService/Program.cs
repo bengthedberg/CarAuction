fd4b04e84810d5bd2bfa2b6d6967bd1eef736076
Bengt Hedberg <bengt.hedberg@gmail.com>
2024-02-18 18:39:34 +1000

    Make the SearchService less dependent on AuctionService.

--- src/CarAction.SearchService/Program.cs
+++ src/CarAction.SearchService/Program.cs
@@ -1,11 +1,16 @@
+using System.Net;
+
 using CarAction.SearchService;
+
+using Polly;
+using Polly.Extensions.Http;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddHttpClient<AuctionServiceHttpClient>();
+builder.Services.AddHttpClient<AuctionServiceHttpClient>().AddPolicyHandler(GetPolicy());
 
 var app = builder.Build();
 
@@ -13,14 +18,21 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-try
+// Ensure that the search service is started even though no successful response have been
+// received from the Auction Service.
+app.Lifetime.ApplicationStarted.Register(async () =>
 {
-    await DbInitializer.InitializeDb(app);
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-    throw;
-}
+    await Policy.Handle<TimeoutException>()
+        .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(10))
+        .ExecuteAndCaptureAsync(async () => await DbInitializer.InitializeDb(app));
+
+});
 
 app.Run();
+
+// Keep trying to get the data from the AuctionService, until we receive a successful response.
+static IAsyncPolicy<HttpResponseMessage> GetPolicy()
+    => HttpPolicyExtensions
+        .HandleTransientHttpError()
+        .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+        .WaitAndRetryForeverAsync(_ => TimeSpan.FromSeconds(3));
