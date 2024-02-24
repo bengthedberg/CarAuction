d573d106150159b5287884868fad30cfd6598ac5
Bengt Hedberg <bengt.hedberg@gmail.com>
2024-02-24 15:00:23 +1000

    Add a registration page

---
+++ src/IdentityService/Pages/Account/Register/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IdentityService.Pages.Register;
+
+public class RegisterViewModel
+{
+    [Required]
+    public string? Email { get; set; }
+    [Required]
+    public string? Password { get; set; }
+    [Required]
+    public string? UserName { get; set; }
+    [Required]
+    public string FullName { get; set; } = string.Empty;
+    public string ReturnUrl { get; set; } = string.Empty;
+    public string? Button { get; set; } = string.Empty;
+
+}
