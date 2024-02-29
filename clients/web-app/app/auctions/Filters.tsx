2c6bc58266e631b98264ddb66a0e5b86a4e4e91a
Bengt Hedberg <bengt.hedberg@gmail.com>
2024-02-29 21:45:39 +1000

    Consistent themes.

--- clients/web-app/app/auctions/Filters.tsx
+++ clients/web-app/app/auctions/Filters.tsx
@@ -1,4 +1,4 @@
-import { ButtonGroup, Button } from "flowbite-react";
+import { ButtonGroup, Button, CustomFlowbiteTheme } from "flowbite-react";
 import React from "react";
 
 type Props = {
@@ -8,18 +8,34 @@ type Props = {
 
 const pageSizeButtons = [4, 8, 12];
 
+const customTheme: CustomFlowbiteTheme["button"] = {
+  color: {
+    gray: "text-gray-500 bg-white border border-gray-300 enabled:hover:bg-gray-100 enabled:hover:text-cyan-700 focus:text-cyan-700 dark:bg-transparent dark:text-gray-400 dark:border-gray-600 dark:enabled:hover:text-white dark:enabled:hover:bg-gray-700",
+    blue: "text-blue-100 bg-blue-700 border border-transparent enabled:hover:bg-gray-100 enabled:hover:text-gray-700 dark:bg-blue-600 dark:hover:bg-blue-700",
+  },
+  size: {
+    custom: "text-base px-4 py-1",
+    xs: "text-xs px-2 py-1",
+    sm: "text-xs px-3 py-1.5",
+    md: "text-sm px-4 py-2",
+    lg: "text-base px-5 py-2.5",
+    xl: "text-base px-6 py-3",
+  },
+};
+
 export default function Filters({ pageSize, setPageSize }: Props) {
   return (
-    <div className="flex justify-between ite4ms-center mb-4">
+    <div className="flex justify-between items-center mb-4">
       <div>
-        <span className="uppercase text-sm text-gray-500 mr-2">Page Size</span>
+        <span className="text-sm text-gray-500 mr-2">Page Size</span>
         <Button.Group>
           {pageSizeButtons.map((value, i) => (
             <Button
               key={i}
               onClick={() => setPageSize(value)}
               color={`${pageSize === value ? "blue" : "gray"}`}
-              className="focus:ring-0"
+              size="custom"
+              theme={customTheme}
             >
               {value}
             </Button>
--- clients/web-app/app/components/AppPagination.tsx
+++ clients/web-app/app/components/AppPagination.tsx
@@ -1,12 +1,40 @@
 "use client";
 
-import { Pagination } from "flowbite-react";
+import { CustomFlowbiteTheme, Pagination } from "flowbite-react";
 import React, { useState } from "react";
 
 type Props = {
   currentPage: number;
   pageCount: number;
   pageChanged: (page: number) => void;
+};
+
+const customTheme: CustomFlowbiteTheme["pagination"] = {
+  base: "",
+  layout: {
+    table: {
+      base: "text-gray-700 dark:text-gray-400",
+      span: "font-semibold text-gray-900 dark:text-white",
+    },
+  },
+  pages: {
+    base: "xs:mt-0 mt-2 inline-flex items-center -space-x-px",
+    showIcon: "inline-flex",
+    previous: {
+      base: "ml-0 rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 enabled:hover:bg-gray-100 enabled:hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 enabled:dark:hover:bg-gray-700 enabled:dark:hover:text-white",
+      icon: "h-5 w-5",
+    },
+    next: {
+      base: "rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-500 enabled:hover:bg-gray-100 enabled:hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 enabled:dark:hover:bg-gray-700 enabled:dark:hover:text-white",
+      icon: "h-5 w-5",
+    },
+    selector: {
+      base: "w-12 border border-gray-300 bg-white py-2 leading-tight text-gray-500 enabled:hover:bg-gray-100 enabled:hover:text-gray-700 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-400 enabled:dark:hover:bg-gray-700 enabled:dark:hover:text-white",
+      active:
+        "bg-blue-700 text-blue-100 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white",
+      disabled: "opacity-50 cursor-normal",
+    },
+  },
 };
 
 export default function AppPagination({
@@ -21,7 +49,7 @@ export default function AppPagination({
       totalPages={pageCount}
       layout="pagination"
       showIcons={true}
-      className="text-blue-500 mb-5"
+      theme={customTheme}
     />
   );
 }
