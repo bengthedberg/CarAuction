3fb692cc362203958c8b2ffb60482fda1b516f80
bengt hedberg <bengt.hedberg@minordkl.com.au>
2024-02-29 16:40:53 +1000

    Setup server functions in client components

--- clients/web-app/app/components/AppPagination.tsx
+++ clients/web-app/app/components/AppPagination.tsx
@@ -6,19 +6,22 @@ import React, { useState } from "react";
 type Props = {
   currentPage: number;
   pageCount: number;
+  pageChanged: (page: number) => void;
 };
 
-export default function AppPagination({ currentPage, pageCount }: Props) {
-  const [pageNumber, setPageNumber] = useState(currentPage);
-
+export default function AppPagination({
+  currentPage,
+  pageCount,
+  pageChanged,
+}: Props) {
   return (
     <Pagination
-      currentPage={pageNumber}
-      onPageChange={(e) => setPageNumber(e)}
+      currentPage={currentPage}
+      onPageChange={(e) => pageChanged(e)}
       totalPages={pageCount}
       layout="pagination"
       showIcons={true}
-      className="text-blue-500 mb-5 "
+      className="text-blue-500 mb-5"
     />
   );
 }
