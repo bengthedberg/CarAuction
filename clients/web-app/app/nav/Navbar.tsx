3c858f0df0bfd3b00944d768563cc78a9804bd6b
Bengt Hedberg <bengt.hedberg@gmail.com>
2024-03-01 20:43:10 +1000

    Add Reset

--- clients/web-app/app/nav/Navbar.tsx
+++ clients/web-app/app/nav/Navbar.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { AiOutlineCar } from "react-icons/ai";
 import Search from "./Search";
+import Logo from "./Logo";
 
 export default function Navbar() {
   return (
     <header className="sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-sm">
-      <div className="flex items-center gap-2 text-3xl font-semibold text-blue-700">
-        <AiOutlineCar size={34} />
-        <div>Car Auctions</div>
-      </div>
+      <Logo />
       <Search />
       <div>Login</div>
     </header>
