6def86e51541e9b15afd25c231986043a694611b
Bengt Hedberg <bengt.hedberg@gmail.com>
2024-03-02 12:15:28 +1000

    Styling

--- clients/web-app/app/nav/LoginButton.tsx
+++ clients/web-app/app/nav/LoginButton.tsx
@@ -1,13 +1,29 @@
 "use client";
 
-import { Button } from "flowbite-react";
+import { Button, CustomFlowbiteTheme } from "flowbite-react";
 import React from "react";
 import { signIn } from "next-auth/react";
+
+const customTheme: CustomFlowbiteTheme["button"] = {
+  color: {
+    gray: "text-gray-500 bg-white border border-gray-300 enabled:hover:bg-gray-100 enabled:hover:text-cyan-700 focus:text-cyan-700 dark:bg-transparent dark:text-gray-400 dark:border-gray-600 dark:enabled:hover:text-white dark:enabled:hover:bg-gray-700",
+    blue: "text-blue-100 bg-blue-500 border border-transparent enabled:hover:bg-gray-100 enabled:hover:text-gray-700 dark:bg-blue-500 dark:hover:bg-blue-700",
+  },
+  size: {
+    custom: "text-base px-4 py-1",
+    xs: "text-xs px-2 py-1",
+    sm: "text-xs px-3 py-1.5",
+    md: "text-sm px-4 py-2",
+    lg: "text-base px-5 py-2.5",
+    xl: "text-base px-6 py-3",
+  },
+};
 
 export default function LoginButton() {
   return (
     <Button
-      outline
+      color="blue"
+      theme={customTheme}
       onClick={() =>
         signIn("id-server", { callbackUrl: "/" }, { prompt: "login" })
       }
