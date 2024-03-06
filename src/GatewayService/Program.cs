71b8d87f57f99d686ea1e9157e75e8bb35bcf005
Bengt Hedberg <bengt.hedberg@gmail.com>
2024-03-06 12:48:53 +1000

    Dockerize Notification service

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -14,7 +14,18 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         options.TokenValidationParameters.NameClaimType = "username"; // Specify what claim is the name of the user
     });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("customPolicy", b =>
+    {
+        b.AllowAnyHeader()
+            .AllowAnyMethod().AllowCredentials().WithOrigins(builder.Configuration["ClientApp"]);
+    });
+});
+
 var app = builder.Build();
+
+app.UseCors();
 
 app.MapReverseProxy();
 
