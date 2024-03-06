3536efb7e260e86f6289c9f3ac432e32e7997c2e
Bengt Hedberg <bengt.hedberg@gmail.com>
2024-03-06 13:46:26 +1000

    Refactor the auction to use Zustand store

--- clients/web-app/app/auctions/Listings.tsx
+++ clients/web-app/app/auctions/Listings.tsx
@@ -9,9 +9,11 @@ import Filters from "./Filters";
 import { useParamsStore } from "@/hooks/useParamsStore";
 import qs from "query-string";
 import EmptyFilter from "../components/EmptyFilter";
+import { useAuctionStore } from "@/hooks/useAuctionStore";
 
 export default function Listings() {
-  const [data, setData] = useState<PagedResult<Auction>>();
+  const [loading, setLoading] = useState(true);
+  const [isServer, setIsServer] = useState(true);
   const params = useParamsStore((state) => ({
     pageNumber: state.pageNumber,
     pageSize: state.pageSize,
@@ -21,6 +23,12 @@ export default function Listings() {
     seller: state.seller,
     winner: state.winner,
   }));
+  const data = useAuctionStore((state) => ({
+    auctions: state.auctions,
+    totalCount: state.totalCount,
+    pageCount: state.pageCount,
+  }));
+  const setData = useAuctionStore((state) => state.setData);
   const setParams = useParamsStore((state) => state.setParams);
   const url = qs.stringifyUrl({ url: "", query: params });
 
@@ -31,10 +39,11 @@ export default function Listings() {
   useEffect(() => {
     getData(url).then((data) => {
       setData(data);
+      setLoading(false);
     });
   }, [url]);
 
-  if (!data) return <h3>loading...</h3>;
+  if (loading) return <h3>loading...</h3>;
 
   return (
     <>
@@ -44,7 +53,7 @@ export default function Listings() {
       ) : (
         <>
           <div className="grid grid-cols-4 gap-6">
-            {data.results.map((auction) => (
+            {data.auctions.map((auction) => (
               <AuctionCard auction={auction} key={auction.id} />
             ))}
           </div>
